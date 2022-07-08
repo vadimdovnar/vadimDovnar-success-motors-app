b572780c877dfc66fdeee0ea8e4828db4190f9b8
vadimdovnar <84572478+vadimdovnar@users.noreply.github.com>
2022-07-08 19:58:26 +0300

    update lwc query whit callback

--- force-app/main/default/lwc/dV_EmailForClient/dV_EmailForClient.js
+++ force-app/main/default/lwc/dV_EmailForClient/dV_EmailForClient.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track} from 'lwc';
+import getEmailTemplates from '@salesforce/apex/DV_EmailForClientController.getEmailTemplates';
+ 
+export default class DV_EmailForClient extends LightningElement {
 
-export default class DV_EmailForClient extends LightningElement {}
+    @track template = {};
+
+    async connectedCallback() {
+        this.template = await getEmailTemplates();
+    }
+
+    get tempSubj() {
+        return this.template.Subject;
+    }
+
+    get tempBody() {
+        return this.template.Body;
+    }
+
+    get contactName() {
+        return this.template.Subject;
+    }
+
+    get contactEmail() {
+        return this.template.Subject;
+    }
+
+    
+}
+
