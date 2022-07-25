10673c0d79e5a2bfe5dcea0c8e25f299757d1243
vadimdovnar <84572478+vadimdovnar@users.noreply.github.com>
2022-07-25 20:24:15 +0300

    create apex test clases

---
+++ force-app/main/default/lwc/propertyTileList/propertyTileList.js
@@ -0,0 +1,10 @@
+import { LightningElement, wire, track } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
+
+
+
+
+export default class PropertyTileList extends LightningElement {
+    
+}
---
+++ force-app/main/default/lwc/sampleDataImporter/sampleDataImporter.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+export default class SampleDataImporter extends LightningElement {
+    
+}
